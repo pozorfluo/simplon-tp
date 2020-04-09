b03e0f9f4f66920bad8c71f1567bf55417979423
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-04-09 12:46:01 +0200

    Add front-end-prairie ex1, ex2

--- front-end-prairie/scripts/ex-playground.sh
+++ front-end-prairie/scripts/ex-playground.sh
@@ -7,11 +7,67 @@
 TITLE="title ex1"
 cd ex1
 
+
+# ex2
+cd ../ex2
+unzip articles.zip
+
+TITLE="title ex2"
+
+CONTENT=$(cat <<CONTENT_HTML
+<header>
+    <nav>
+        <h1>header title</h1>
+        <ul>
+            <li><a href="#">header nav</a></li>
+            <li><a href="#">header nav</a></li>
+            <li><a href="#">header nav</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main role=”main”>
+    <article>
+        <header>
+            <h2>article title</h2>
+            <p>tagline</p>
+            <a><img src="" /></a>
+        </header>
+        <p>article body</p>
+    </article>
+
+    <aside>
+        <details>
+            <summary>article extra</summary>
+            <ul>
+                <li><a href="#">extras</a></li>
+                <li><a href="#">extras</a></li>
+            </ul>
+        </details>
+    </aside>
+</main>
+
+<footer>
+    <h2>footer title</h2>
+        <ul>
+            <li><a href="#">footer link</a></li>
+            <li><a href="#">footer link</a></li>
+            <li><a href="#">footer link</a></li>
+        </ul>
+    <address>
+        <a href="mailto:mail@example.com">contact</a>
+    </address>
+</footer>
+CONTENT_HTML
+)
+echo "$CONTENT"
+
 # setup basic project structure
 mkdir www && cd www
 mkdir html
 mkdir css
 mkdir js
+mkdir images
 touch index.html
 touch js/script.js
 touch css/style.css
@@ -20,13 +76,14 @@ cat <<MINIMAL_HTML > index.html
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>$TITLE</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css" />
     <script src="script.js"></script>
   </head>
   <body>
-    <!-- page content -->
+    $CONTENT
   </body>
 </html>
 MINIMAL_HTML
