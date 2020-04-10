7f3317438bff9224ed54f86c9d66e88662fb6faf
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-04-10 10:43:07 +0200

    Setup initial json data manipulation plumbing

--- Projet-allo-cine/js/main.js
+++ Projet-allo-cine/js/main.js
@@ -5,10 +5,12 @@
    * Plumbing test
    */
   function changeInput(domElement, input) {
-
     domElement.textContent = input ? `Hello, ${input}!` : "...";
   }
 
+   /**
+   * window loaded !
+   */
   window.onload = (event) => {
     const domElement = document.getElementById("output");
     const input = document.getElementById("input");
@@ -20,5 +22,26 @@
       },
       false
     );
+
+    //-------------------------------------------- initial json plumbing ---
+    let request_url =
+      "https://pozorfluo.github.io/simplon-tp/Projet-allo-cine/data/moovies.json";
+
+    let request = new XMLHttpRequest();
+
+    request.open('GET', request_url);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        const movies = request.response;
+        // const movie_count = movies.length;
+        changeInput(domElement, movies[0].Title);
+
+        for (const [index, movie] of movies.entries()) {
+            console.log(movie.Title);
+        }
+    }
+      
   };
 })();
