e7961d973c59b0a99514ee4e0de1e6413fead01e
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-04-24 01:05:47 +0200

    Use vmin for square size in vanilla-checker exercise

--- prairie/hello-mysql/docker_playground.sh
+++ prairie/hello-mysql/docker_playground.sh
@@ -11,6 +11,14 @@ sudo nohup docker daemon -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock
 # docker run hello-world
 # docker image ls
 
+# may or may not be useful
+    export DOCKER_HOST=127.0.0.1
+    # see https://gist.github.com/styblope/dc55e0ad2a9848f2cc3307d4819d819f
+    service --status-all
+    # change MySQL Port in devilbox .env file
+    HOST_PORT_MYSQL=33060
+    # see https://deliciousbrains.com/devilbox-local-wordpress-development-docker/
+
 # say hello to devilbox
 git clone https://github.com/cytopia/devilbox
 cd devilbox
@@ -39,3 +47,38 @@ mysql -u root -h 127.0.0.1 -p -e 'CREATE DATABASE my_wp;'
 
 # configure dns
 sudo nano /etc/hosts
+
+# say hello to php
+cd ~/devilbox/data/www
+mkdir hello-php/htdocs
+
+# add to C:\Windows\System32\drivers\etc\hosts
+127.0.0.1 hello-php.loc
+
+# configure vs code for php debug and devilbox
+# see https://devilbox.readthedocs.io/en/latest/intermediate/configure-php-xdebug/toolbox/vscode.html
+# see also creating a .devcontainer/devcontainer.json file
+
+# install php7.0
+sudo apt-get install php7.0
+# The following NEW packages will be installed:
+#   apache2 apache2-bin apache2-data apache2-utils libapache2-mod-php7.0 libapr1 libaprutil1 libaprutil1-dbd-sqlite3 libaprutil1-ldap liblua5.2-0 php-common php7.0
+#   php7.0-cli php7.0-common php7.0-json php7.0-opcache php7.0-readline psmisc ssl-cert
+
+
+# setup basic index.php
+cat <<MINIMAL_HTML > index.php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>$TITLE</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="js/script.js"></script>
+  </head>
+  <body>
+    $CONTENT
+  </body>
+</html>
+MINIMAL_HTML
